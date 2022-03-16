36616a259a5051c68f7a0e2f28288cae9759e7d7
James Pretorius <jamesmpretorius@gmail.com>
2022-03-15 22:58:04 -0400

    searchmanager stub

---
+++ src/main/java/SearchManager.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+public class SearchManager {
+    private Filter filter;
+    private Search search;
+
+    public SearchManager() {}
+
+    public void setSearch(Search search) {}
+
+    public List<Listing> getSearchResults() {
+        return new ArrayList<Listing>();
+    }
+
+    public void addFilter(Filter filter) {}
+
+    public void removeFilter(Filter filter) {}
+
+    public Filter getFilters() {}
+}
