b4a15c8484a483f15a942736e4a93d665ce86838
Jack H <jhyatt7331@gmail.com>
2022-03-17 13:46:31 -0400

    Fixing flight stub

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,17 +1,34 @@
 import java.time.LocalDate;
 
-public class Flight {
+/**
+ * @author Jack Hyatt
+ */
+public class Flight extends TravelObject{
     private LocalDate departureTime;
     private LocalDate arivalTime;
 
     /**
      * Constructor for Flight
      */
-    public Flight(LocalDate departureTime, LocalDate arivalTime) {
+    public Flight(LocalDate departureTime, LocalDate arivalTime,BookingLayout layout) {
+        super(layout);
         this.departureTime = departureTime;
         this.arivalTime = arivalTime;
     }
-    
 
+    /**
+     * 
+     * @return the departure time of the flight as a LocalDate data type
+     */
+    public LocalDate getDepartureTime() {
+        return this.departureTime;
+    }
 
+    /**
+     * 
+     * @return the arival time of the flight as a LocalDate data type
+     */
+    public LocalDate getArivalTime() {
+        return this.arivalTime;
+    }
 }
