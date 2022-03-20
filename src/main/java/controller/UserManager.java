4d0b22cfa81f675cb71b0a0cf8776244955a58e3
Renato Gonzalez-Tapia <RENATOG@email.sc.edu>
2022-03-20 17:07:03 -0400

    Pseudocode and additional method stubs/doccumentation for User and UserManager

--- src/main/java/controller/UserManager.java
+++ src/main/java/controller/UserManager.java
@@ -2,20 +2,39 @@ package controller;
 
 import database.Data;
 import model.users.User;
-
 import java.util.List;
+
 /**
- * Handles all user related tasks
+ * Linked list that handles user related tasks
+ * For sanity, users are currently beind stored: [     ]
+ * 
+ * @author rengotap
  */
 public class UserManager {
-    private User currentUser;
-    private List<User> users;
+
+    private User loggedIn; // save data here
+    private User currentUser; // curr user in LL
+    private List<User> users; // LL
 
     /**
      * Creates a new Controllor.UserManager
      * @param data
      */
     public UserManager(Data data) {
+        // ====PSEUDOCODE====
+
+        // ---STARTUP(list)---
+        // add all users to the user list, not sure why but the UML says so, easy enough
+
+        // ---STARTUP(temp)---
+        // check to see if user "temp" exists
+        // if it does (from a bad exit), wipe and create a new one.
+        // temp will be a guest account 
+
+        // ---LOGIN/SEARCH---
+        // if nobody is logged in create a new guest account to store prefrences
+        // If someone logs in Login() and set loggedIn
+        // wipe "temp" and write to the new account
 
     }
 
@@ -26,7 +45,18 @@ public class UserManager {
      * @return login accepted (T/F)
      */
     public boolean login(String username, String password) {
-        return false;
+        // Checking to see if the user can access the data
+        if (saveDataExists(username))
+            return false;
+        if (!passwordCorrect(username, password))
+            return false;
+        // Accessing the user data
+
+        // TODO: actually set the user data
+
+        // Sets loggedIn as the logged in user
+        loggedIn = new User(person, username, password);
+        return true;
     }
 
     /**
@@ -34,7 +64,7 @@ public class UserManager {
      * @param user user to register
      */
     public void registerUser(User user) {
-
+        // TODO: registerUser
     }
 
     /**
@@ -42,7 +72,7 @@ public class UserManager {
      * @param user user to remove
      */
     public void unregisterUser(User user) {
-
+        // TODO: unregisterUser
     }
 
     /**
@@ -52,4 +82,26 @@ public class UserManager {
     public User getCurrentUser() {
         return currentUser;
     }
+
+    /**
+     * Checks to see if the user exists in the database
+     * @param username
+     * @return a file for user data exists(T/F)
+     */
+    public boolean saveDataExists(String username) {
+        //TODO: check save data
+        return false;
+    }
+
+    /**
+     * Checks login credentials before allowing the user to be created
+     * @param username user's name
+     * @param password user's secret password
+     * @return username password combo is correct
+     */
+    public boolean passwordCorrect(String username, String password) {
+        //TODO: read password
+        return false;
+    }
+
 }
--- src/main/java/model/users/User.java
+++ src/main/java/model/users/User.java
@@ -1,14 +1,15 @@
 package model.users;
 
-
+import model.bookables.TravelObject;
 import model.users.info.Passport;
 import model.users.info.Person;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Saves all user related data
+ * All user related data
+ * Edit this, then write it to the file
+ * @author rengotap
  */
 public class User {
     private String username;
@@ -18,28 +19,110 @@ public class User {
     private List<Passport> travelers;
     private Person person;
 
-    public User(Person person, String username, String password) {}
+    /**
+     * Creates a new User
+     * !! Data will always be read from file !!
+     * @param person name
+     * @param username username
+     * @param password password
+     */
+    public User(Person person, String username, String password) {
 
-    public void addTraveler(Passport passport) {}
+    }
 
-    public void removeTraveler(Passport passport) {}
+    /**
+     * Creates a new temporary User account
+     * Used for saving temporary guest data
+     * !! Guest data should not be saved* !!
+     * 
+     * *Can be converted into a regular account and saved 
+     *  if the user desires using registerUser() method
+     */
+    public User() {
+        username = "temp";
+        password = "temp";
+    }
 
+    /**
+     * Takes temporary guest data and saves it as a registered user for later
+     * @param username new username
+     * @param password new password
+     * @return True if operation was successful
+     */
+    public boolean registerUser(String username, String password) {
+        // === PSEUDO CODE === 
+        // check to see if specified requested username is available
+        // assign new user selected username and password
+        // set LoggedIn in UserManager to be the current user, (should be taken care of?)
+        this.username = username;
+        this.password = password;
+        //TODO: Check if username is available
+        return false;
+    }
+    /**
+     * Adds a traveler (family)
+     * @param passport
+     */
+    public void addTraveler(Passport passport) {
+        //TODO: add traveler to travelers
+    }
+
+    /**
+     * Removes a traveler (family)
+     * @param passport
+     */
+    public void removeTraveler(Passport passport) {
+        //TODO: remove traveler from travelers
+    }
+
+    /**
+     * Gives a list of all travelers associated with this account
+     * @return Array list of travelers
+     */
     public List<Passport> getTravelers() {
         return new ArrayList<Passport>();
     }
 
     /**
      * Gets the user associated with the account
+     * @return returns the account owner
      */
     public Person getRegisteredPerson() {
         return person;
     }
 
+    /**
+     * Gets the user's Email
+     * @return email string
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Changes the user's Email
+     * @param email new email to set
+     */
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    // User Booking History Methods
+
+    /**
+     * Saves a previous booking
+     * For booking flights/reservations
+     * @param toAdd
+     */
+    public void addBooking(TravelObject toAdd) {
+        // TODO: add booking to json history
+    }
+
+    /**
+     * Removes a previous booking
+     * For canceling flights/reservations
+     */
+    public void removeBooking() {
+        // TODO: remove booking form json history
     }
 }
