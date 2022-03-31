68d9c201c2c618fc1380ea075338c9cbf0194c28
JimmyPinkard <jimmypinkard7@gmail.com>
2022-03-30 20:45:29 -0400

    Got reading and writing working

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -1,5 +1,4 @@
 mongo --eval 'db.getMongo().getDBNames().forEach(function(i){db.getSiblingDB(i).dropDatabase()})'
 mongoimport --db Flighty --collection Flights --jsonArray --file database/flights.json
 mongoimport --db Flighty --collection Hotels --jsonArray --file database/hotels.json
-mongoimport --db Flighty --collection Users --jsonArray --file database/userdata/coleca.json
-mongoimport --db Flighty --collection Users --jsonArray --file database/userdata/realHughMann.json
+mongoimport --db Flighty --collection Users --jsonArray --file database/users.json
