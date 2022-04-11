023d1737ff5360f35865322d0498c2aa7c1bd057
Jimmy Pinkard <jimmypinkard7@gmail.com>
2022-04-10 22:09:45 -0400

    Valid Test Cases for Data

--- src/test/java/database/DataTest.java
+++ src/test/java/database/DataTest.java
@@ -1,32 +1,35 @@
 package database;
 
+import model.Booking;
 import model.bookables.flight.Flight;
+import model.bookables.hotel.Hotel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DataTest {
 
+    Data data = DatabaseData.getInstance();
+
     @Test
     void testValidFlight() {
-        var data = DatabaseData.getInstance();
-        var flight = data.getFlights().get(0);
-        try {
-            for(Field field : Flight.class.getFields()) {
-                Assertions.assertNotNull(field.get(flight));
-            }
-        }
-        catch (Exception e) {
-            System.err.println(e.getMessage());
+        for(Flight flight : data.getFlights()) {
+            Assertions.assertNotNull(flight);
         }
     }
 
     @Test
-    void testNullFlight() {
-        var data = DatabaseData.getInstance();
-        var flight = data.getFlights().get(0);
+    void testValidHotel() {
+        for(Hotel hotel : data.getHotels()) {
+            Assertions.assertNotNull(hotel);
+        }
+    }
+
+    @Test
+    void testValidBookings() {
+        for(Booking booking : data.getBookings()) {
+            Assertions.assertNotNull(booking);
+        }
     }
 }
